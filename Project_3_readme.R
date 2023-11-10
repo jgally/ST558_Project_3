3a8d055bfdfcb7b981367b286e912ba4dd268181
kerenvm26 <134894786+kerenvm26@users.noreply.github.com>
2023-11-10 10:41:08 -0500

    Update Project_3_readme.R

--- Project_3_readme.R
+++ Project_3_readme.R
@@ -13,7 +13,8 @@ library(readr)     # To read data into R
 library(dplyr)     # To clean data
 library(ggplot2)   # To plot graphs
 library(caret)     # To create predictive models and test them
-library (shinny)   # To automate the creation of 5 github_documents
+library(shinny)    # To automate the creation of 5 github_documents
+library(purr)      # 
 
 # Render the R Markdown file to README.md
 
